a7c975404d7d0a796a462bb35aa0c4871260b051
Marchal Maxence <maxence.marchal@gmail.com>
2024-10-07 18:48:20 +0200

    push

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
 
-defineBackend({
+import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
 });
+
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
